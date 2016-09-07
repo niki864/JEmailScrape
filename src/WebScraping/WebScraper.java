2b9ae246ba949b175cc47f0283c48b0f6831363f
niki864 <skhselikin@gmail.com>
2016-09-07 11:14:49 +0530

    Initial Working Set

---
+++ src/WebScraping/WebScraper.java
@@ -0,0 +1,42 @@
+package WebScraping;
+
+import java.io.*;
+import java.net.*;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+ 
+public class WebScraper {
+ 
+  public static void main(String[] args) {
+    try {
+      // fetch the document over HTTP
+    	URL url = new URL("http://www.nitt.edu/");
+    	HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
+    	String line = null;
+    	StringBuilder tmp = new StringBuilder();
+    	BufferedReader in = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+    	while ((line = in.readLine()) != null) {
+    	  tmp.append(line);
+    	}
+    	 
+    	Document doc = Jsoup.parse(tmp.toString());
+      
+      // get the page title
+      String title = doc.title();
+      System.out.println("title: " + title);
+      
+      // get all links in page
+      Elements links = doc.select("a[href]");
+      for (Element link : links) {
+        // get the value from the href attribute
+        System.out.println("\nlink: " + link.attr("href"));
+        System.out.println("text: " + link.text());
+      }
+    } catch (IOException e) {
+    e.printStackTrace();
+    }
+  }
+}
